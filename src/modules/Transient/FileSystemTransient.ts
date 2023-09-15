ba606faa21cfa843bf78157d3fdd3fffa5c9a628
Yami-no-karuro <carloborgna98@gmail.com>
2023-09-15 22:03:01 +0200

    FileSystemTransient updates

--- src/modules/Transient/FileSystemTransient.ts
+++ src/modules/Transient/FileSystemTransient.ts
@@ -33,27 +33,46 @@ export default class FileSystemTransient extends AbstractTransient {
 
   /**
    * @package VectorJS
-   * FileSystemTransient.getContent()
+   * FileSystemTransient.getContentSync()
    * @returns Object | undefined 
    */
-  public getContent(): Object | undefined {
+  public getContentSync(): Object | undefined {
     let content: TransientData | undefined = this.content;
     if (undefined === content) {
-      content = JSON.parse(
-        fs.readFileSync('/app/var/transients/' + this.name, 'utf8')
-      );
+      content = JSON.parse(fs.readFileSync('/app/var/transients/' + this.name, 'utf8'));
+      this.content = content;
     }
     return content?.content;
   }
 
   /**
    * @package VectorJS
-   * FileSystemTransient.setContent()
+   * FileSystemTransient.getContent()
+   * @returns Promise<Object | undefined>
+   */
+  public getContent(): Promise<Object | undefined> {
+    return new Promise(async (resolve, _) => {
+      let content: TransientData | undefined = this.content;
+      if (undefined === content) {
+        fs.readFile('/app/var/transients/' + this.name, (error, content) => {
+          if (!error) {
+            this.content = JSON.parse(content.toString());
+            resolve(this.content?.content);
+          }
+          resolve(undefined);
+        });
+      }
+    });
+  }
+
+  /**
+   * @package VectorJS
+   * FileSystemTransient.setContentSync()
    * @param content: Object
    * @param ttl: number
    * @returns void 
    */
-  public setContent(content: Object, ttl: number): void {
+  public setContentSync(content: Object, ttl: number): void {
     const date: Date = new Date();
     const createdAt: number = date.getTime();
     const data: TransientData = {
@@ -64,6 +83,28 @@ export default class FileSystemTransient extends AbstractTransient {
     fs.writeFileSync('/app/var/transients/' + this.name,
       JSON.stringify(data), 'utf8'
     );
+  }
+
+  /**
+   * @package VectorJS
+   * FileSystemTransient.setContent()
+   * @param content: Object
+   * @param ttl: number
+   * @returns Promise<void>
+   */
+  public setContent(content: Object, ttl: number): Promise<void> {
+    return new Promise((resolve, _) => {
+      const date: Date = new Date();
+      const createdAt: number = date.getTime();
+      const data: TransientData = {
+        createdAt: createdAt,
+        ttl: ttl,
+        content: content
+      }
+      fs.writeFile('/app/var/transients/' + this.name, JSON.stringify(data), (_) => {
+        resolve();
+      });
+    });
   }
 
 }
