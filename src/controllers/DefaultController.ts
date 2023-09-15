f69f6d16b8f300a931803d16ddd80c5a567dba34
Yami-no-karuro <carloborgna98@gmail.com>
2023-09-15 18:38:51 +0200

    Refactoring + Transients

--- src/controllers/DefaultController.ts
+++ src/controllers/DefaultController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { AbstractController } from './AbstractController';
+import { AbstractController } from '../modules/Controller/AbstractController';
 
 export class DefaultController extends AbstractController {
 
